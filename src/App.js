81506b2a82043b9c73e773c788d66e43f5820630
Jose6348 <jfalasco88@gmail.com>
2025-03-25 21:30:12 -0300

    feat: integra componentes e adiciona Font Awesome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import CategoryTags from './components/CategoryTags';
+import ProductCards from './components/ProductCards';
+import ImageGrid from './components/ImageGrid';
+import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    line-height: 1.6;
+    background-color: #f5f5f5;
+  }
+
+  :root {
+    --primary-color: #8a2be2;
+    --text-color: #333;
+    --background-dark: #1a1a1a;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Hero />
+      <CategoryTags />
+      <ProductCards />
+      <ImageGrid />
+      <ContactForm />
+      <Footer />
+    </>
   );
 }
 
